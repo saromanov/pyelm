f7c294af3880ee171b8b60559b711513db3cd9e7
Sergey Romanov <xxsmotur@gmail.com>
2015-01-22 23:49:14 +0500

    Still to implement

--- example.py
+++ example.py
@@ -1,10 +1,17 @@
 from pyelm import ELM
 
-def test_elm():
+def test_elm1():
 	inp = [([1,0,0,1],[1,0]), ([0,1,0,0],[0,0]), ([1,1,1,1], [1,0]), ([1,0,0,0], [0,1])]
 	elm = ELM(inp,7)
 	elm.train(10)
 	#elm.train(5, 10, [[0.74,0.88,0,145], [.2,1,0,.3], [1,0.74,1,1], [1,0,0,0]])
+
+
+def test_elm2():
+	X = np.random.randn(5,5)
+	y = np.random.randint(2, size=(2,2))
+	elm = ELM([X,y], 2)
+	elm.train(50)
 
 
 test_elm()
--- pyelm.py
+++ pyelm.py
@@ -1,9 +1,33 @@
 import theano
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
-import theano.tensor.nlinalg
+from theano.tensor.nlinalg import MatrixPinv
 import numpy as np
 
+
+
+class HiddenLayer:
+	def __init__(self, inp, num_visible, num_hidd, bias, weights_inphid=None, weights_hidout=None):
+		self.weights_inphid = weights_inphid
+		self.weights_hidout = weights_hidout
+		self.bias = bias
+		self.vis = inp
+		if self.weights_inphid == None:
+			self.weights_inphid = theano.shared(
+			np.array(
+				np.random.random((num_vis, num_hidd))
+				), name='Wih')
+		if self.weights_hidout == None:
+			self.weights_hidout = theano.shared(
+			np.array(
+				np.random.random((num_hidd, num_out))
+				), name='Who')
+
+		hidden_pre = T.nnet.sigmoid(T.dot(self.vis, self.weights_inphid) + self.bias)
+		self.result = self._inverse(self.weights_hidout, hidden_pre)
+
+	def _inverse(self, value, outdata):
+		return T.dot(outdata, MatrixPinv()(value))
 
 class ELM:
 	""" 
@@ -11,35 +35,31 @@ class ELM:
 		num_hidd - number of hidden neurons
 	"""
 	def __init__(self, visible, num_hidd):
-		self.inpdata = list(map(lambda x: x[0], visible))
-		self.outdata = list(map(lambda x: x[1], visible))
+		self.inpdata = visible[0]
+		self.outdata = visible[1]
 		stream = RandomStreams()
-		num_vis = len(self.inpdata)
-		num_out = len(self.outdata)
+		self.num_vis = len(self.inpdata)
+		self.num_out = len(self.outdata)
+		self.num_hidd = num_hidd
 		self.vis = T.dmatrix('vis')
 		self.outd = T.dmatrix('out')
 		#init as weights vector connected from input layer to hidden
 		self.weights_inphid = theano.shared(
 			np.array(
-				np.random.random((num_vis, num_hidd))
+				np.random.random((self.num_vis, self.num_hidd))
 				), name='Wih')
-
-		#init as weights vector connected from hidden layer to output
+		#main training param
 		self.weights_hidout = theano.shared(
 			np.array(
-				np.random.random((num_hidd, num_out))
+				np.random.random((self.num_hidd, self.num_out))
 				), name='Who')
-		self.bias = theano.shared(np.ones(num_hidd, dtype=theano.config.floatX))
+		self.bias = theano.shared(np.ones(self.num_hidd, dtype=theano.config.floatX))
 
 	def train(self, iters):
 		""" TODO: write function with scan and updates """
-		hidden_pre = T.dot(self.vis, self.weights_inphid) + self.bias
-		hidden = T.dot(self.weights_hidout, T.nnet.sigmoid(T.dot(hidden_pre, self.weights_hidout)))
-		result = self._inverse(hidden, self.outd)
-
-	def _inverse(self, value, outdata):
-		"""
-			Moore-Penrose pseudo-inverse of a matrix
-		"""
-		#pinv_result = theano.shared(np.linalg.pinv(value))
-		return T.dot(outdata, MatrixPinv(value))
+		hidden = HiddenLayer(self.vis, self.num_vis, self.num_hidd, self.bias, weights_inphid=self.weights_inphid,\
+			weights_hidout=self.weights_hidout)
+		diff = hidden.result - self.outd.T
+		#cost = T.sum(T.dot(diff, diff))
+		func = theano.function([self.vis, self.outd], diff)
+		res = func(self.inpdata, self.outdata)
