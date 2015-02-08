2ce237da3972cf2aad92b66b060f7e8aaee92020
Sergey Romanov <xxsmotur@gmail.com>
2015-02-08 23:57:55 +0500

    Finish to implement

--- pyelm.py
+++ pyelm.py
@@ -7,38 +7,28 @@ import numpy as np
 
 
 class HiddenLayer:
-	def __init__(self, inp, num_visible, num_hidd, bias, weights_inphid=None, weights_hidout=None):
+	def __init__(self, inp, num_visible, num_hidd, bias, weights_inphid=None):
 		self.weights_inphid = weights_inphid
-		self.weights_hidout = weights_hidout
 		self.bias = bias
 		self.vis = inp
 		if self.weights_inphid == None:
 			self.weights_inphid = theano.shared(
 			np.array(
-				np.random.random((num_vis, num_hidd))
+				np.random.random((num_visible, num_hidd))
 				), name='Wih')
-		if self.weights_hidout == None:
-			self.weights_hidout = theano.shared(
-			np.array(
-				np.random.random((num_hidd, num_out))
-				), name='Who')
-
-		hidden_pre = T.nnet.sigmoid(T.dot(self.vis, self.weights_inphid) + self.bias)
-		self.result = self._inverse(self.weights_hidout, hidden_pre)
-
-	def _inverse(self, value, outdata):
-		return T.dot(outdata, MatrixPinv()(value))
+		self.hidden_pre = T.nnet.sigmoid(T.dot(self.vis, self.weights_inphid) + self.bias)
+		self.result = MatrixPinv()(self.hidden_pre)
 
 class ELM:
 	""" 
 		Visible - input data as pair (x,y)
 		num_hidd - number of hidden neurons
 	"""
-	def __init__(self, visible, num_hidd):
+	def __init__(self, visible, num_vis, num_hidd):
 		self.inpdata = visible[0]
 		self.outdata = visible[1]
 		stream = RandomStreams()
-		self.num_vis = len(self.inpdata)
+		self.num_vis = num_vis
 		self.num_out = len(self.outdata)
 		self.num_hidd = num_hidd
 		self.vis = T.dmatrix('vis')
@@ -48,18 +38,28 @@ class ELM:
 			np.array(
 				np.random.random((self.num_vis, self.num_hidd))
 				), name='Wih')
-		#main training param
-		self.weights_hidout = theano.shared(
-			np.array(
-				np.random.random((self.num_hidd, self.num_out))
-				), name='Who')
 		self.bias = theano.shared(np.ones(self.num_hidd, dtype=theano.config.floatX))
 
-	def train(self, iters):
+	def _cost(self, x,y, method='lse'):
+		return T.sum(T.sqrt((x - y)**2))/x.shape[0]
+
+	def _trainInner(self):
+		hidden = HiddenLayer(self.vis, self.num_vis, self.num_hidd, self.bias, weights_inphid=None)
+		self.weights_hidout = T.dot(hidden.result, self.outd)
+		value = T.dot(hidden.hidden_pre, self.weights_hidout)
+		loss = self._cost(self.outd, value)
+		return loss
+
+	def train(self):
 		""" TODO: write function with scan and updates """
-		hidden = HiddenLayer(self.vis, self.num_vis, self.num_hidd, self.bias, weights_inphid=self.weights_inphid,\
-			weights_hidout=self.weights_hidout)
-		diff = hidden.result - self.outd.T
-		#cost = T.sum(T.dot(diff, diff))
-		func = theano.function([self.vis, self.outd], diff)
-		res = func(self.inpdata, self.outdata)
+		loss = self._trainInner()
+		func = theano.function([self.vis, self.outd], loss)
+		return loss
+
+	def predict(self, X):
+		x = T.matrix('x')
+		layer = HiddenLayer(x, X.shape[1], 0, self.bias, weights_inphid=self.weights_inphid)
+		result = T.dot(layer.result.T, self.weights_hidout)
+		func = theano.function([x, self.vis, self.outd], result)
+		return func(X, self.inpdata, self.outdata)
+
